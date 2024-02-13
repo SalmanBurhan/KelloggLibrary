e5aa85e62f6b1cd08361e2e494db70b9dc9a39db
Salman Burhan <salmanfburhan@gmail.com>
2024-02-13 10:25:47 -0800

    Initial Commit

---
+++ KelloggLibrary/KelloggLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KelloggLibraryApp.swift
+//  KelloggLibrary
+//
+//  Created by Salman Burhan on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct KelloggLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
