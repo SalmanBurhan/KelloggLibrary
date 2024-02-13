63565dde21cc620565fecb4bde35ae72281fb2c0
Salman Burhan <salmanfburhan@gmail.com>
2024-02-13 11:44:44 -0800

    Add ordinal functionality to Int

--- KelloggLibrary/Extensions/Int+Extensions.swift
+++ KelloggLibrary/Extensions/Int+Extensions.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+/// This extension provides additional functionality to `Int`.
+extension Int {
+  /// Returns the ordinal representation of the integer.
+  ///
+  /// For example, 1 becomes "1st", 2 becomes "2nd", 3 becomes "3rd", and so on.
+  ///
+  /// - Returns: The ordinal representation of the integer.
+  var ordinal: String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .ordinal
+    let number = NSNumber(integerLiteral: self)
+    return formatter.string(from: number) ?? "\(self)"
+  }
+}
